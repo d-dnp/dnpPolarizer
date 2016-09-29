48a17f31c008418bb22839f149543c5194f1111f
Benno Meier <b.meier@soton.ac.uk>
2016-09-29 16:24:51 +0100

    Changes to software/mercuryMonitor.py

--- software/mercuryMonitor.py
+++ software/mercuryMonitor.py
@@ -12,8 +12,15 @@ tC = mercuryITC.temperatureControllerEthernet()
 temp = tC.getSignal("mb1", "TEMP")
 print "Temp: ", temp
 
+logfilename = "201609xxlog.txt"
+logfile = open(logfilename, "w")
+logfile.write("Time \t  T Cryostat \t T Sample\n======================================")
+logfile.close()
+
 while True:
     try:
+        print time.time()
+        
         temp1 = tC.getSignal("mb1", "TEMP")
         print "Cryostat Temperature: ", temp1
 
@@ -21,6 +28,11 @@ while True:
         print "Probe Temperature: ", temp2
 
         print "\n"
+
+        logfile = open(logfilename, "a")
+        logfile.write("\n" + str(time.time()) + "\t" + str(temp1) + "\t" + str(temp2))
+        logfile.close()
+
         time.sleep(30)
         
     except KeyboardInterrupt:
